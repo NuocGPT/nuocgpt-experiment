e9ce7ac0be6238d68cf21c8b6d5cb21e7466bcf1
Minh Tue Vo Thanh <minhtuev@gmail.com>
2023-08-09 15:53:53 -0700

    Integrate llhama index to main web app

--- server.py
+++ server.py
@@ -7,40 +7,19 @@ from flask import Flask, request, jsonify, render_template
 import openai
 from openai.error import ServiceUnavailableError
 from pymongo import MongoClient, errors
-
-uri = os.environ.get("MONGODB_URI")
-print("MongoDB URI", uri)
-cluster = MongoClient(uri)
-db = cluster["NuocDB"]
-collections = db.list_collection_names()
-
-# Try to ping the deployment and handle any exceptions that arise
-try:
-    cluster.admin.command("ping")
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except errors.ConnectionFailure as e:
-    print(f"Failed to connect to MongoDB: {e}")
+from llama_index import StorageContext, load_index_from_storage
 
 
 app = Flask(__name__)
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-
-# Check if API key works
-completion = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="Hello world")
-print(completion.choices[0].text)
-
 # Vars and constants
-app.user_message = ""
-app.assistant_response = ""
 TOPICS = ["politics", "vietnamese history", "vietnam war"]
 SYSTEM_INSTRUCTION = f"You are NuocGPT, a conversational AI designed to answer questions about climate and water" \
                      f" issues in Vietnam. You can only answer questions in Vietnamese or English. You will not" \
                      f" comment or answer any questions related to these topics: {TOPICS}."
 GPT3 = "gpt-3.5-turbo"
 GPT4 = "gpt-4"
+
 
 @app.route("/")
 def home():
@@ -144,35 +123,44 @@ def chat():
     # Call the OpenAI API to get a response
     try:
         print(user_message)
-        if user_message:
-            messages = [
-                {"role": "system", "content": SYSTEM_INSTRUCTION},
-                {"role": "user", "content": user_message},
-            ]
+
+        # if the pattern starts with "Query:", we just query the index; this is a test pattern for the index
+        if "Query:" in user_message:
+            query_str = user_message.replace("Query:", "")
+            final_response = app.query_engine.query(query_str)
         else:
-            messages = [{"role": "system", "content": SYSTEM_INSTRUCTION}]
+            if user_message:
+                context_response = app.query_engine.query(user_message)
+                user_message = f"Additional context information: {context_response}\nMy question: {user_message}"
 
-        response = openai.ChatCompletion.create(
-            model=GPT4,
-            messages=messages,
-            max_tokens=1000,
-            temperature=0.7,
-            n=1,
-            stop=None,
-        )
-        # Extract the response text from the API response
-        print(response)
-        assistant_response = response["choices"][0]["message"]["content"].strip(
-        )
+                messages = [
+                    {"role": "system", "content": SYSTEM_INSTRUCTION},
+                    {"role": "user", "content": user_message},
+                ]
+            else:
+                messages = [{"role": "system", "content": SYSTEM_INSTRUCTION}]
 
-        messages.append({"role": "assistant", "content": assistant_response})
+            response = openai.ChatCompletion.create(
+                model=GPT4,
+                messages=messages,
+                max_tokens=1000,
+                temperature=0.7,
+                n=1,
+                stop=None,
+            )
+            # Extract the response text from the API response
+            print(response)
+            assistant_response = response["choices"][0]["message"]["content"].strip(
+            )
 
-        critiqued_response = critique(messages, "passthrough")
+            messages.append({"role": "assistant", "content": assistant_response})
+
+            final_response = critique(messages, "passthrough")
 
         app.user_message = user_message
-        app.assistant_response = critiqued_response
+        app.assistant_response = final_response
 
-        return render_template("index.html", response=critiqued_response)
+        return render_template("index.html", response=final_response)
     except ServiceUnavailableError:
         return render_template(
             "index.html",
@@ -189,7 +177,7 @@ def feedback():
     :return: A response indicating the feedback was received.
     """
     reaction = request.json.get("reaction")
-    collection = db["ResponseLog"]
+    collection = app.db["ResponseLog"]
     # Store the reaction in the MongoDB collection
     feedback_data = {
         "id": "reaction",
@@ -205,6 +193,46 @@ def feedback():
     return jsonify({"message": "Feedback received"})
 
 
+def init_openai_key():
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+    # Check if API key works
+    completion = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Hello world")
+    print(completion.choices[0].text)
+
+
+def load_mongo_db():
+    uri = os.environ.get("MONGODB_URI")
+    print("MongoDB URI", uri)
+    cluster = MongoClient(uri)
+    mongo_db = cluster["NuocDB"]
+    collections = mongo_db.list_collection_names()
+
+    # Try to ping the deployment and handle any exceptions that arise
+    try:
+        cluster.admin.command("ping")
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        print("Available collections:", collections)
+    except errors.ConnectionFailure as failure_exception:
+        print(f"Failed to connect to MongoDB: {failure_exception}")
+    return mongo_db
+
+
+def load_llhama_index():
+    storage_dir = os.environ.get("LLHAMA_INDEX", 'preliminary-llama-index')
+    storage_context = StorageContext.from_defaults(persist_dir=storage_dir)
+    index = load_index_from_storage(storage_context, 'vector_index')
+    return index
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5010))
+    init_openai_key()
+    app.db = load_mongo_db()
+    app.index = load_llhama_index()
+    app.query_engine = app.index.as_query_engine()
+    app.user_message = ""
+    app.assistant_response = ""
     app.run(host="0.0.0.0", port=port)
